a1cc8cb96d278afd238b2ff5e3b693ae3865d12c
Ramon Campos <ramoncampos2202@outlook.com>
2022-10-31 16:33:27 -0300

    atualizacao 5

--- Formularios/cadastrarCliente.php
+++ Formularios/cadastrarCliente.php
@@ -15,9 +15,6 @@
         ?>
 
         <?php    
-            // abre conexão com o banco
-            require_once '../conexao.php';
-            
             // recebe os dados do formulário]
 
             $nomeCliente = $_POST['nomeCliente'];
@@ -41,21 +38,50 @@
 
             if(isset($INSERIR_CLIENTE) && !empty($INSERIR_CLIENTE)){
                 try{
-                    $sql = "INSERT INTO tb_clientes (nome_cliente,end_cliente,cep_cliente,tel_cliente,cpf_cliente,tipo_pagamento,dt_nasc)
-                    VALUES('".$nomeCliente."',
-                    '".$endCliente."',
-                    '".$cepCliente."',
-                    '".$telCliente."',
-                    '".$cpfCliente."',
-                    '".$tpPagamento."',
-                    '".$dtNasc."')";
+                    // abre conexão com o banco
+                    require_once '../conexao.php';
+                    
+                    // recebe os dados do formulário]
+            
+                    $nomeCliente = $_POST['nomeCliente'];
+                    $endCliente = $_POST['endCliente'];
+                    $cepCliente = $_POST['cepCliente'];
+                    $telCliente = $_POST['telCliente'];
+                    $cpfCliente = $_POST['cpfCliente'];
+                    $tpPagamento = $_POST['tpPagamento'];
+                    $dtNasc = $_POST['dtNasc'];
+        
+                //  var_dump($_POST);
+                //  exit();
+        
+                // verifica se já existe um registro na tabela para o código informado (chave duplicada)
 
-                    $stmt = $conn->prepare($sql);
+                    $result = $conn->query("SELECT * FROM tb_clientes where cpf_cliente = '$cpfCliente'");
+        
+                    $count = $result->rowCount();
+                    if ($count > 0) {
+                        $destino = "function () {window.location='./Formularios/cadastrarCliente.php';}";
+                        echo "<script>sendToastr('Código de cliente já cadastrado!<br />Clique para continuar!','error',$destino)</script>";
+                    }
+        
+                    $stmt = $conn->prepare('INSERT INTO tb_clientes VALUES	(:nome_cliente,:end_cliente,:cep_cliente,:tel_cliente,:cpf_cliente,:tipo_pagamento,:dt_nasc)');
+                    $stmt->bindValue(':nome_cliente', $nomeCliente);
+                    $stmt->bindValue(':end_cliente', $endCliente);
+                    $stmt->bindValue(':cep_cliente', $cepCliente);
+                    $stmt->bindValue(':tel_cliente', $telCliente);
+                    $stmt->bindValue(':cpf_cliente', $cpfCliente);
+                    $stmt->bindValue(':tipo_pagamento', $tpPagamento);
+                    $stmt->bindValue(':dt_nasc', $dt_nasc);
+                    // $sql = "INSERT INTO tb_clientes (nome_cliente,end_cliente,cep_cliente,tel_cliente,cpf_cliente,tipo_pagamento,dt_nasc)VALUES ('$nomeCliente','$endCliente','$cepCliente','$telCliente','$cpfCliente','$if(isset($INSERIR_CLIENTE) && !empty($INSERIR_CLIENTE)){tpPagamento','$dtNasc')";
+        
+                    // $stmt = $conn->prepare($sql);
+                        
                     $stmt->execute();
                     
                 }catch (PDOException $e) {
                     // caso ocorra uma exceção, exibe na tela
                 
+        
                     $destino = "function () {window.location='cadastrarCliente.php';}";
                     echo "<script>'error',$destino)</script>";
                     die();
@@ -64,18 +90,18 @@
 
 
 
-            if( isset($ALTERAR)){     
-                alterar($id,$valor,$valor);    
-            }
+            // if( isset($ALTERAR)){     
+            //     alterar($id,$valor,$valor);    
+            // }
 
-            if (isset($DELETAR)) {  
-                deletar($id);   
-            }
+            // if (isset($DELETAR)) {  
+            //     deletar($id);   
+            // }
 
         ?>
 
         <div class='card-body'>
-            <form action='' method='POST'>
+            <form action='cadastrarCliente.php' method='POST'>
                 <div class="form-group">
                     <label>Nome :</label>
                     <input type="text" class="form-control" name="nomeCliente" id="nomeCliente" placeholder="Nome" required />
--- dados.php
+++ dados.php
@@ -1,47 +1,9 @@
 <?php    
-    // abre conexão com o banco
-    require_once 'conexao.php';
     
-    // recebe os dados do formulário]
 
-    $nomeCliente = $_POST['nomeCliente'];
-    $endCliente = $_POST['endCliente'];
-    $cepCliente = $_POST['cepCliente'];
-    $telCliente = $_POST['telCliente'];
-    $cpfCliente = $_POST['cpfCliente'];
-    $tpPagamento = $_POST['tpPagamento'];
-    $dtNasc = $_POST['dtNasc'];
-
-    //  var_dump($_POST);
-    //  exit();
-
-    // verifica se já existe um registro na tabela para o código informado (chave duplicada)		
-		 $result = $conn->query("SELECT * FROM tb_clientes where cpf_cliente = '$cpfCliente'");
-
-		 $count = $result->rowCount();
-		 if ($count > 0) {
-		 	$destino = "function () {window.location='./Formularios/cadastrarCliente.php';}";
-		 	echo "<script>sendToastr('Código de cliente já cadastrado!<br />Clique para continuar!','error',$destino)</script>";
-		 }
-     
-
-    if(isset($INSERIR_CLIENTE) && !empty($INSERIR_CLIENTE)){
-        try{
-            $sql = "INSERT INTO tb_clientes (nome_cliente,end_cliente,cep_cliente,tel_cliente,cpf_cliente,tipo_pagamento,dt_nasc)VALUES ('$nomeCliente','$endCliente','$cepCliente','$telCliente','$cpfCliente','$tpPagamento','$dtNasc')";
-
-            $stmt = $conn->prepare($sql);
-                
-            $stmt->execute();
-            
-        }catch (PDOException $e) {
-            // caso ocorra uma exceção, exibe na tela
         
-
-            $destino = "function () {window.location='cadastrarCliente.php';}";
-            echo "<script>'error',$destino)</script>";
-            die();
-        }
-    } 
+        
+    
 
 
 
